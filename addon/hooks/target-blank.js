19884183e1d89fa50e1023af89cca18042c9064f
Alex LaFroscia <alex@lafroscia.com>
2018-04-09 15:14:21 -0700

    Add `noopener` to built-in `target-blank` hook
    Since it's considered dangerous to use `target="_blank"` without `rel="noopener"` I think the built-in transform should do the safe thing automatically.

--- addon/hooks/target-blank.js
+++ addon/hooks/target-blank.js
@@ -4,6 +4,7 @@ export default class TargetBlankHook extends Hook {
   afterSanitizeAttributes(node) {
     if (node instanceof HTMLAnchorElement) {
       node.setAttribute('target', '_blank');
+      node.setAttribute('rel', 'noopener');
     }
   }
 }
