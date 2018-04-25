423ee04b805b31a2f77a9c8ee67d4bb6df9de5e4
Jason Mitchell <jason.mitchell.w@gmail.com>
2018-04-25 11:53:25 -0700

    Bundling sourcemap file to assets folder

--- index.js
+++ index.js
@@ -5,8 +5,11 @@ module.exports = {
 
   included() {
     this._super.included.apply(this, arguments);
+
     this.import('node_modules/dompurify/dist/purify.js', {
       using: [{ transformation: 'amd', as: 'dompurify' }]
     });
+
+    this.import('node_modules/dompurify/dist/purify.js.map', { destDir: 'assets' });
   }
 };
