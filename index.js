7afd3fd475d404f2e3a6f54b01a7ec338adc5e82
Jason Mitchell <jason.mitchell.w@gmail.com>
2018-06-20 14:15:09 -0700

    Remove sourceMappingURL for dom-purify

--- index.js
+++ index.js
@@ -1,15 +1,61 @@
 'use strict';
 
+const path = require('path');
+const funnel = require('broccoli-funnel');
+const stringReplace = require('broccoli-string-replace');
+const UnwatchedDir = require('broccoli-source').UnwatchedDir;
+
 module.exports = {
   name: 'ember-dompurify',
 
   included() {
     this._super.included.apply(this, arguments);
 
-    this.import('node_modules/dompurify/dist/purify.js', {
+    this.import('vendor/purify.js', {
       using: [{ transformation: 'amd', as: 'dompurify' }]
     });
+  },
 
-    this.import('node_modules/dompurify/dist/purify.js.map', { destDir: 'assets' });
+  findModulePath(basedir) {
+    try {
+      let resolve = require('resolve');
+
+      return path.dirname(resolve.sync('dompurify', { basedir: basedir }));
+    } catch (_) {
+      try {
+        return path.dirname(require.resolve('dompurify'));
+      } catch (e) {
+        if (e.code === 'MODULE_NOT_FOUND') {
+          this.ui.writeLine(
+            `ember-dompurify: dompurify not installed, be sure you have dompurify installed via npm/yarn.`
+          );
+          return;
+        }
+
+        throw e;
+      }
+    }
+  },
+
+  removeSourcemapAnnotation(tree) {
+    return stringReplace(tree, {
+      files: ['purify.js'],
+      annotation: 'Remove sourcemap annotation (dom-purify)',
+      patterns: [
+        {
+          match: /\/\/# sourceMappingURL=purify.js.map/g,
+          replacement: ''
+        }
+      ]
+    });
+  },
+
+  treeForVendor() {
+    let purifyPath = this.findModulePath(this.project.root);
+    let tree = funnel(new UnwatchedDir(purifyPath), {
+      include: ['purify.js']
+    });
+
+    return this.removeSourcemapAnnotation(tree);
   }
 };
